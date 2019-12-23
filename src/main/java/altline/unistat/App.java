86e2c3413985e27becec366d900c14a6e030a6f3
domyz <domyz@DOMY-PC>
2019-12-23 19:15:18 +0100

    v1.0.10 release

--- src/main/java/altline/unistat/App.java
+++ src/main/java/altline/unistat/App.java
@@ -33,7 +33,7 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 	public static final String APPNAME = "UniStat";
-	public static final String VERSION = "1.0.10_pre";
+	public static final String VERSION = "1.0.10";
 	public static final String AUTHOR = "AltLine";
 	public static final String TITLE = APPNAME + " - v" + VERSION;
 	public static final boolean DEBUG_MODE = false;
